97fe85a6478806bb18052801e25f96a7edfca826
torinunna <torinunna9588@gmail.com>
2023-10-30 17:13:59 +0900

    implement operation methods

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -52,20 +52,63 @@ class ViewController: UIViewController {
             self.outputLabel.text = self.displayNumber
         }
     }
-  
+    
     @IBAction func divideBtnPressed(_ sender: UIButton) {
+        self.operation(.Divide)
     }
     
     @IBAction func multiplyBtnPressed(_ sender: UIButton) {
+        self.operation(.Multiply)
     }
     
     @IBAction func addBtnPressed(_ sender: UIButton) {
+        self.operation(.Add)
     }
     
     @IBAction func subtractBtnPressed(_ sender: UIButton) {
+        self.operation(.Subtract)
     }
     
     @IBAction func equalBtnPressed(_ sender: UIButton) {
+        self.operation(self.currentOperation)
+    }
+    
+    func operation(_ operation: Operation) {
+        if self.currentOperation != .unknown {
+            if !self.displayNumber.isEmpty {
+                self.secondOperand = self.displayNumber
+                self.displayNumber = ""
+                
+                guard let firstOperand = Double(self.firstOperand) else { return }
+                guard let secondOperand = Double(self.secondOperand) else { return }
+                
+                switch self.currentOperation {
+                case .Add:
+                    self.result = "\(firstOperand + secondOperand)"
+                case .Subtract:
+                    self.result = "\(firstOperand - secondOperand)"
+                case .Multiply:
+                    self.result = "\(firstOperand * secondOperand)"
+                case .Divide:
+                    self.result = "\(firstOperand / secondOperand)"
+                default:
+                    break
+                }
+                
+                if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0 {
+                    self.result = "\(Int(result))"
+                }
+                
+                self.firstOperand = self.result
+                self.outputLabel.text = self.result
+            }
+            
+            self.currentOperation = operation
+        } else {
+            self.firstOperand = self.displayNumber
+            self.currentOperation = operation
+            self.displayNumber = ""
+        }
     }
 }
 
